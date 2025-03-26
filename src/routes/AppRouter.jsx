4f8c258d6bdb50a716a23d8aebbbf7438b84a957
Mohammed Khalid <thekhalidsha@gmail.com>
2025-03-26 10:43:20 +0530

    `Update import statement in AppRouter.jsx to use HashRouter instead of BrowserRouter`

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, HashRouter as BrowserRouter } from 'react-router-dom'
 import Login from '../pages/Login'
 import PublicLayout from './PublicLayout'
 import PrivateLayout from './PrivateLayout'
